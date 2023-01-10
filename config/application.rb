c5ee2fb30bee6bb9edee1a92d51e89525320e2c3
Austin Miller <austinmillerwebdev@gmail.com>
2023-01-10 13:37:11 -0500

    Add flickr ID as global constant

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module RailsFlickr
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Access with Rails.configuration.flickr_id
     config.flickr_id = "197387511@N07"
 
     # Configuration for the application, engines, and railties goes here.
